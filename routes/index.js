348981fd82ed4a868d72070cd253c482428b040d
Michael Velcev <badboy1664@gmail.com>
2020-09-16 01:17:06 +0300

    Derp. Fix missing constant post rename

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 
 const getCircularReplacer = require('../helpers/getCircularReplacer');
-
-router.get('', (req, res, next) => {
+const callback = (req, res, next) => {
   const object = {
     req: JSON.parse(JSON.stringify(req, getCircularReplacer())),
     res: JSON.parse(JSON.stringify(res, getCircularReplacer())),
@@ -10,6 +9,12 @@ router.get('', (req, res, next) => {
 
   console.log(object);
   res.status(200).json(object);
-});
+};
+
+router.get('', callback);
+router.post('', callback);
+router.put('', callback);
+router.delete('', callback);
+router.option('', callback);
 
 module.exports = router;
